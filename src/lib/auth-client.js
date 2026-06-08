dee22f13dc3dd6c9fd907de21ee4e5ec6d5261c9
saiful-islam-soaus-45 <soausahmedbd91@gmail.com>
2026-06-08 13:49:54 +0600

    Our final project issue has been solved

--- src/lib/auth-client.js
+++ src/lib/auth-client.js
@@ -1,5 +1,5 @@
 import { createAuthClient } from "better-auth/react"
 export const authClient = createAuthClient({
-    baseURL: "http://localhost:3000"
+    baseURL: "https://assignment-8-nine-sigma.vercel.app"
 })
 export default authClient;
