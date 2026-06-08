d8a77d1ed831cf4604876bff95bf4a780075f429
saiful-islam-soaus-45 <soausahmedbd91@gmail.com>
2026-06-08 13:21:18 +0600

    fix: project ready for deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,24 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  /* config options here */
-  reactCompiler: true,
 
+/** @type {import('next').Next} */
+const nextConfig = {
+  output: 'standalone',
   experimental: {
-    serverComponentsExternalPackages: ["@better-auth/kysely-adapter"],
+    serverComponentsExternalPackages: ['@better-auth/kysely-adapter', 'kysely'],
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+        port: '',
+        pathname: '**',
+        search: '',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.pexels.com',
+      }
+    ],
   },
 };
 
